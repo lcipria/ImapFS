f422403b4b744b9bb24d709b7f694a6c2c4d398a
Luciano Cipria <lcipria@comune.genova.it>
2024-07-30 12:22:42 +0200

    initial support for RW

--- ImapFS.py
+++ ImapFS.py
@@ -1,11 +1,12 @@
-import config
-import os
-import sys
-import imaplib
-import email
-import time
-import re
+from email import message, message_from_string
 from fuse import FUSE, FuseOSError, Operations
+import config
+import errno
+import imaplib
+import os
+import re
+import sys
+import time
 
 class ImapFS(Operations):
     def __init__(self):
@@ -13,13 +14,13 @@ class ImapFS(Operations):
         resp_code, response = self.client.login(config.mail_username, config.mail_password)
 
     def getattr(self, path, fh=None):
-        print(f'getattr({path}, {fh}')
+        print(f'getattr({path}, {fh})')
         if self.__path_is_folder__(path):
             return {
                 'st_atime': 0,
                 'st_ctime': 0,
                 'st_mtime': 0,
-                'st_mode': 0o40555,
+                'st_mode': 0o40777,
                 'st_nlink': 2,
                 'st_size': 4096,
                 'st_uid': 0,
@@ -30,7 +31,7 @@ class ImapFS(Operations):
                 'st_atime': 0,
                 'st_ctime': 0,
                 'st_mtime': 0,
-                'st_mode': 0o100444,
+                'st_mode': 0o100666,
                 'st_nlink': 1,
                 'st_size': 4096, # correggere
                 'st_uid': 0,
@@ -40,8 +41,8 @@ class ImapFS(Operations):
     def readdir(self, path, fh):
         print(f'readdir({path}, {fh})')
         if self.__path_is_folder__(path):
-            normpath = os.path.normpath(f'{config.mail_folder}/{path}')
-            base_depth = normpath.count('/') + 1
+            normpath = self.__normpath__(path)
+            base_depth = normpath.count('/')
             for entry in self.client.list(normpath)[1]:
                 if entry:
                     for folder in re.findall('(?<="/" ").+(?="$)', entry.decode()):
@@ -56,19 +57,111 @@ class ImapFS(Operations):
         if self.__path_is_file__(path):
             resp_code, response = self.client.fetch('1', '(RFC822)')
             if resp_code.lower() == 'ok':
-                block = email.message_from_string(response[0][1].decode())
-                return block.get_payload().encode('utf-8')#[offset:offset + size]
+                block = message_from_string(response[0][1].decode())
+                return block.get_payload().encode('utf-8')[offset:offset + size]
 
     def write(self, path, data, offset, fh):
         print(f'write({path}, data, {offset}, {fh})')
         if self.__path_exists__(path):
-            self.client.delete(os.path.normpath(f'{config.mail_folder}/{path}'))
-        self.client.create(os.path.normpath(f'{config.mail_folder}/{path}'))
-        self.__create_block__(path, data)
+            self.client.delete(self.__normpath__(path))
+        self.client.create(self.__normpath__(path))
+
+        block = message.Message()
+        block.set_payload(data)
+        self.client.append(
+            self.__normpath__(path),
+            '',
+            imaplib.Time2Internaldate(time.time()),
+            str(block).encode("utf-8"))
+
         return len(data)
 
+    def access(self, path, mode):
+        print(f'access({path}, {mode})')
+
+    def chmod(self, path, mode):
+        print(f'chmod({path}, {mode})')
+
+    def chown(self, path, uid, gid):
+        print(f'chown({path}, {uid}, {gid})')
+
+    def create(self, path, mode, fi=None):
+        print(f'create({path}, {mode}, {fi})')
+
+    def flush(self, path, fh):
+        print(f'flush({path}, {fh})')
+
+    def fsync(self, path, fdatasync, fh):
+        print(f'fsync({path}, {fdatasync}, {fh})')
+
+    def link(self, target, name):
+        print(f'link({target}, {name})')
+
+    def mkdir(self, path, mode):
+        print(f'mkdir({path}, {mode})')
+        if not self.__path_exists__(path):
+            self.client.create(self.__normpath__(path))
+
+    def mknod(self, path, mode, dev):
+        print(f'mknod({path}, {mode}, {dev})')
+
+    def open(self, path, flags):
+        print(f'open({path}, {flags})')
+        accmode = flags & os.O_ACCMODE
+        if accmode == os.O_RDONLY:
+            if self.__path_is_file__(path):
+                return 0
+            else:
+                return -errno.ENOENT
+        elif accmode == os.O_WRONLY:
+            if flags & os.O_APPEND:
+                print("append")
+            if self.__path_is_file__(path):
+                return 0
+            else:
+                return -errno.ENOENT
+
+        elif accmode == os.O_RDWR:
+            if flags & os.O_APPEND:
+                print("append")
+            return 0
+        else:
+            return -errno.EACCES
+
+    def readlink(self, path):
+        print(f'readlink({path})')
+
+    def release(self, path, fh):
+        print(f'release({path}, {fh})')
+
+    def rename(self, old, new):
+        print(f'rename({old}, {new})')
+
+    def rmdir(self, path):
+        print(f'rmdir({path})')
+        if self.__path_is_folder__(path):
+            self.client.delete(self.__normpath__(path))
+
+    def statfs(self, path):
+        print(f'statfs({path})')
+
+    def symlink(self, name, target):
+        print(f'symlink({name}, {target})')
+
+    def truncate(self, path, length, fh=None):
+        print(f'truncate({path}, {length}, {fh})')
+
+    def unlink(self, path):
+        print(f'unlink({path})')
+        if self.__path_is_file__(path):
+            self.client.delete(self.__normpath__(path))
+
+    def utimens(self, path, times=None):
+        print(f'utimens({path}, {times})')
+
     def __check_path__(self, path):
-        return self.client.select(os.path.normpath(f'{config.mail_folder}/{path}'))
+        normpath = self.__normpath__(path)
+        return self.client.select(normpath)
 
     def __path_exists__(self, path):
         resp_code, response = self.__check_path__(path)
@@ -78,20 +171,16 @@ class ImapFS(Operations):
         resp_code, response = self.__check_path__(path)
         if resp_code.lower() == 'ok':
             return int(response[0]) == 0
+        return False
 
     def __path_is_file__(self, path):
         resp_code, response = self.__check_path__(path)
         if resp_code.lower() == 'ok':
             return int(response[0])
+        return False
 
-    def __create_block__(self, path, data):
-        block = email.message.Message()
-        block.set_payload(data)
-        self.client.append(
-            os.path.normpath(f'{config.mail_folder}/{path}'),
-            '',
-            imaplib.Time2Internaldate(time.time()),
-            str(block).encode("utf-8"))
+    def __normpath__(self, path):
+        return os.path.normpath(f'"{config.mail_folder}/{path}"')
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
